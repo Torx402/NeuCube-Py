314c2aac3dd23552c6c19cc972ccbfbff2d41404
Torx402 <77255180+Torx402@users.noreply.github.com>
2025-01-14 11:12:36 +0100

    Update sampler.py

--- neucube/sampler/sampler.py
+++ neucube/sampler/sampler.py
@@ -1,90 +1,171 @@
 import torch
-from abc import ABC, abstractmethod
-
-class Sampler(ABC):
-    """
-    Abstract base class for sampling state vectors for classification.
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    @abstractmethod
-    def sample(self, X_in):
-        """
-        Abstract method to sample from input data.
-
-        Parameters:
-            X_in: Input data.
-
-        Returns:
-            State vectors.
-        """
-        pass
         
 
-class SpikeCount(Sampler):
+class SpikeCount():
     """
     Sampler that calculates the spike count from spike activity.
     """
 
     def __init__(self):
         super().__init__()
-
-    def sample(self, spike_activity):
+    
+    def __repr__(self):
+        return f"{type(self).__name__}()"
+    
+    def fit(self, X, y=None):
+        pass
+    
+    def transform(self, X, y=None):
         """
         Calculates the spike count from spike activity.
 
         Parameters:
-            spike_activity: Spike activity tensor or array-like object.
+            X: Spike activity tensor or array-like object.
 
         Returns:
             State vectors (spike count for each sample).
         """
-        return spike_activity.sum(axis=1)
+        return X.sum(axis=1)
+    
+    def fit_transform(self, X, y=None):
+        return self.transform(X, y)
+    
+    def get_params(self, deep=None):
+        return self.__dict__
+    
+    def set_params(self, **params):
+        pass
 
-
-class DeSNN(Sampler):
+class DeSNN():
     """
     DeSNN Sampler to sample state vectors based on spike activity.
     """
 
-    def __init__(self, alpha=5, mod=0.8, drift_up=0.8, drift_down=0.01):
+    def __init__(self, alpha=1, mod=0.4, drift=0.25):
         """
         Initializes the DeSNN sampler.
 
         Parameters:
-            alpha: Maximum initial weight (default: 5).
-            mod: Modulation factor for importance of the order of spikes (default: 0.8).
-            drift_up: Drift factor for spikes (default: 0.8).
-            drift_down: Drift factor for no spikes (default: 0.01).
+            alpha: Maximum initial weight (default: 1).
+            mod: Modulation factor for importance of the order of spikes (default: 0.4).
+            drift: Drift factor (default: 0.25).
         """
         self.alpha = alpha
         self.mod = mod
-        self.drift_up = drift_up
-        self.drift_down = drift_down
+        self.drift = drift
 
-    def sample(self, spike_activity):
+    def __repr__(self):
+        return f"{type(self).__name__}(alpha={self.alpha}, mod={self.mod}, drift={self.drift})"
+    
+    def __sklearn_clone__(self):
+        return self
+    
+    def fit(self, X, y=None):
+        pass
+    
+    def transform(self, X, y=None):
         """
         Calculates ranks based on spike activity.
 
         Parameters:
-            spike_activity: Spike activity tensor or array-like object.
+            X: Spike activity tensor or array-like object.
 
         Returns:
             State vectors (DeSNN).
         """
         # Find the index of the first non-zero element along the second axis
-        first_spike = (spike_activity != 0).int().argmax(axis=1)
+        first_spike = (X != 0).int().argmax(axis=1)
 
         # Calculate initial ranks based on the alpha and mod parameters
         initial_ranks = self.alpha * (self.mod ** first_spike)
 
         # Calculate drift_up by multiplying spike activity along the second axis
-        up = self.drift_up * spike_activity.sum(axis=1)
+        up = self.drift * X.sum(axis=1)
 
         # Calculate drift_down by multiplying by no spikes
-        down = self.drift_down * (spike_activity.shape[1] - spike_activity.sum(axis=1))
+        down = self.drift * (X.shape[1] - X.sum(axis=1))
 
         # Return the result of the rank calculation
-        return initial_ranks + up - down
+        return initial_ranks + (up - down)
+    
+    def fit_transform(self, X, y=None):
+        return self.transform(X, y)
+    
+    def get_params(self, deep=None):
+        return {"alpha": self.alpha, "mod": self.mod, "drift": self.drift}
+    
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
+    
+    
+class DeSNNt():
+    """
+    DeSNN Sampler implementation based on https://doi.org/10.1007/s11063-020-10322-8,
+    to sample state vectors based on spike activity.
+    """
+
+    def __init__(self, mod=0.4, drift=0.25):
+        """
+        Initializes the DeSNN sampler.
+
+        Parameters:
+            mod: Modulation factor for importance of the order of spikes (default: 0.4).
+            drift: Drift factor (default: 0.25).
+        """
+        self.mod = mod
+        self.drift = drift
+
+    def __repr__(self):
+        return f"{type(self).__name__}(mod={self.mod}, drift={self.drift})"
+    
+    def __sklearn_clone__(self):
+        return self
+    
+    def fit(self, X, y=None):
+        pass
+    
+    def transform(self, X, y=None):
+        """
+        Calculates ranks based on spike activity.
+
+        Parameters:
+            X: Spike activity tensor or array-like object. (n_samples, n_time, n_neurons)
+
+        Returns:
+            State vectors (DeSNN).
+        """
+        
+        state_vectors = torch.zeros((X.shape[0], X.shape[2]))
+
+        for i in range(X.shape[0]):
+            fire_mat = torch.zeros(X.shape[2])
+            w_mat = torch.zeros(X.shape[2])
+
+            for j in range(X.shape[1]):
+            
+                fired_indices = X[i, j].nonzero().reshape(-1)
+                for idx in fired_indices:
+                    if fire_mat[idx] == 0.:
+                        w_mat[idx] = self.mod ** fire_mat.sum()
+                        fire_mat[idx] = 1.
+                    else:
+                        w_mat[idx] = w_mat[idx] + self.drift
+                
+                w_mat = torch.where(X[i, j] == 0., w_mat - self.drift, w_mat)
+            
+            state_vectors[i] = w_mat 
+        
+        return state_vectors
+    
+    def fit_transform(self, X, y=None):
+        return self.transform(X, y)
+    
+    def get_params(self, deep=None):
+        return {"mod": self.mod, "drift": self.drift}
+    
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
